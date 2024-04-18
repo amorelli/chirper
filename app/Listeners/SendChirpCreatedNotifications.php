888d81250b059b5824526f3aaa3dc301212f99ff
Adam Morelli <morelli@3cisd.com>
2024-04-17 20:51:52 -0400

    Initial commit

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\ChirpCreated  $event
+     * @return void
+     */
+    public function handle(ChirpCreated $event)
+    {
+        // \Log::info("chirp created by "print_r("Chirp created by $event->chirp->user_id", true));
+        // \Log::info("chirp created by ", ["id" => $event->chirp->user_id]);
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
