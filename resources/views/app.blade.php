07e6e9499b9fa0688f2e940388e2e82d08db682b
Adam Morelli <morelli@3cisd.com>
2024-05-03 15:47:35 -0400

    Add Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'AdamsApp') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CNYNJZRWS1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CNYNJZRWS1');
+        </script>
 
         <!-- Scripts -->
         @routes
